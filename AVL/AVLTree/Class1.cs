c30c8c244eed43b4ee6a33d9aad6a4d10cfdccfa
thequeen14 <flamina96@yandex.ru>
2017-02-26 12:06:01 +0300

    Add files via upload

---
+++ AVL/AVLTree/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVLTree
+{
+    public class Node<TKey, TValue>
+    {
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+        public Node<TKey, TValue> Left { get; set; }
+        public Node<TKey, TValue> Right { get; set; }
+        public Node<TKey, TValue> Parent { get; set; }
+
+        public Node(TKey key, TValue value)
+        {
+            Key = key;
+            Value = value;
+            Left = null;
+            Right = null;
+            Parent = null;
+        }
+    }
+
+    public class AVLTree
+    {
+        public int CheckBalanceFactor()
+        {
+            return 1;
+        }
+    }
+}
